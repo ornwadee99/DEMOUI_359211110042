839b910aa4e32eb7fe7e788cf035fe92cbe896dc
ornwadee99 <ornwadee28493@gmail.com>
2018-02-12 16:51:45 +0700

    User Login with sqlite

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class dbConnection {
 
-    private static final String SQCONN = "jdbc:sqlite:src/school.sqlite";
+    private static final String SQCONN = "jdbc:sqlite:\\school.sqlite";
 
     public static Connection getConnection () throws SQLException {
         try {
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,19 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,7 +21,12 @@ public class Controller implements Initializable {
     private loginModel loginmodel = new loginModel();
     @FXML
     private Label dbStatus;
-
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button btnLogin;
 
 
     @Override
@@ -24,7 +38,36 @@ public class Controller implements Initializable {
         }
 
 
-
-
     }//initilize
+
+    @FXML
+    public void Login(ActionEvent event){
+       try{
+           if (this.loginmodel.isLogin(this.username.getText(),this.password.getText())){
+               Stage stage = (Stage) this.btnLogin.getScene().getWindow();
+               stage.close();
+
+               adminLogin();
+
+           }else{
+               JOptionPane.showMessageDialog (null,"Your username or" + "password is not corrected.");
+           }
+       }catch (Exception localExcaption){
+           localExcaption.printStackTrace();
+       }
+
+    }//
+
+    private void adminLogin() {
+        try{
+            Stage adminstage = new Stage();
+            FXMLLoader adminLoader = new FXMLLoader();
+            Pane adminRoot = adminLoader.load(getClass().getResource("/Admin/AdminDashBoard.fxml").openStream());
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+
+    }
+
+
 }
